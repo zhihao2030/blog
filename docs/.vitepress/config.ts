595d86dbb3c5efbd8087513f55113af7f58df6a9
zhihao <zhihao2030@163.com>
2022-11-15 12:25:13 +0800

    feat: cleanUrls: 'without-subfolders',

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   lastUpdated: true,
   base: '/',
   lang: 'zh-CN',
+  cleanUrls: 'without-subfolders',
   head:[
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
