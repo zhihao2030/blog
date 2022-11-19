1a5fcc708b202a8dc3ab67457c1902a767c2b0a0
zhzhang <zzh0660@gmail.com>
2022-11-19 14:34:03 +0800

    feat: Google analytics

--- docs/.vitepress/config/head/index.js
+++ docs/.vitepress/config/head/index.js
@@ -1,4 +1,6 @@
 const headConfig = [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['script', {async: true, src:'https://www.googletagmanager.com/gtag/js?id=G-4H800SD2DB'}],
+    ['script', { src:'/google/analytics.js'}],
 ]
 export default headConfig
---
+++ docs/public/google/analytics.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-4H800SD2DB');
