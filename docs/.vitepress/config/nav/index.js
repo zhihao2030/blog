047554e9d377f1ef261513964f6f8ad22e761fb1
zhihao <zhihao2030@163.com>
2022-11-30 16:18:45 +0800

    feat: 整理nav

--- docs/.vitepress/config/nav/index.js
+++ docs/.vitepress/config/nav/index.js
@@ -4,19 +4,16 @@ const navConfig =  [
     { text: 'Javascript', link: '/javascript/' ,activeMatch: '/javascript/'},
     { text: '网络协议', link: '/network-protocol/',activeMatch: '/network-protocol/' },
     { text: '工程化', link: '/devops/' ,activeMatch: '/devops/'},
-    { text: '浏览器', link: '/browser/' ,activeMatch: '/browser/'},
-    // { text: '算法', link: '/algorithm/' ,activeMatch: '/algorithm/'},
-    // { text: 'React', link: '/react/' ,activeMatch: '/react/'},
     {
         text: '分类',
         link: '/tags',
         activeMatch: '/tags'
     },
-    {
-        text: '归档',
-        link: '/archives',
-        activeMatch: '/archives'
-    },
+    // {
+    //     text: '归档',
+    //     link: '/archives',
+    //     activeMatch: '/archives'
+    // },
 ]
 
 export default navConfig
