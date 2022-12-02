a5ccfe2e3058425d663bef742d3b09cd1cc48f2e
zhihao <zhihao2030@163.com>
2022-12-02 16:29:29 +0800

    fix: ssr渲染时pinia数据持久化会出问题

--- docs/.vitepress/theme/store/index.js
+++ docs/.vitepress/theme/store/index.js
@@ -1,9 +1,9 @@
 import { createPinia } from 'pinia'
-import piniaPersist from 'pinia-plugin-persistedstate'
+//import piniaPersist from 'pinia-plugin-persistedstate'
 //pinia-plugin-persistedstate
 
 const store = createPinia()
-store.use(piniaPersist)
+//store.use(piniaPersist)
 export default function setupStore(app) {
   app.use(store)
 }
