c4e616191530ee252b80c5c6de0f37021e81ce5e
zhzhang <zzh0660@gmail.com>
2022-12-07 17:37:02 +0800

    feat: openAi

--- docs/.vitepress/theme/components/setSession/setSession.vue
+++ docs/.vitepress/theme/components/setSession/setSession.vue
@@ -16,7 +16,7 @@ import {SendOutlined} from '@ant-design/icons-vue'
 const inputValue = ref(null)
 const send = async () =>{
   try {
-    const response = await fetch(`https://1270001.xyz/openAi/session?session=${inputValue.value}`)
+    const response = await fetch(`/openAi/session?session=${inputValue.value}`)
     let {data,flag,msg} = await response.json();
     Message.error(msg)
   } catch(e) {
