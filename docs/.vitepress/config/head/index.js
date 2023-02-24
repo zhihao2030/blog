c5bbe0356ef2c371358d4d64d53ed47772deaf55
zhihao <zhihao2030@163.com>
2023-02-24 12:50:16 +0800

    ADS

--- docs/.vitepress/config/head/index.js
+++ docs/.vitepress/config/head/index.js
@@ -1,6 +1,6 @@
 const headConfig = [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['script', {async: true, src:'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1877146996810982,crossorigin:"anonymous"'}],
+    ['script', {async: true, src:'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1877146996810982',crossorigin:"anonymous"}],
     ['script', {async: true, src:'https://www.googletagmanager.com/gtag/js?id=G-4H800SD2DB'}],
     ['script', {async: true, src:'https://hm.baidu.com/hm.js?8e71fc6c2a4d00b06ee5c2c294e7d573'}],
     ['script', { src:'/google/analytics.js'}],
